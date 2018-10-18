78dda16037d3127b0e9afff362bb68ce429f6e43
edna <edna@rr.com>
2018-10-19 00:02:21 +0300

    modify travis.yml

--- app1/api.py
+++ app1/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -75,6 +75,8 @@ def get_post_sales_record():
 @app.route('/api/v1/sales/<int:record_id>', methods=['GET'])
 def get_single_entry(record_id):
     for record in sales_records:
-        if record['record_id'] == record_id:
+        try:
+            record['record_id'] == record_id
             return jsonify(record), 200
-        return jsonify({'message': "no record with such an id"}), 400
+        except:
+            return jsonify({'message': "no record with such an id"}), 400
