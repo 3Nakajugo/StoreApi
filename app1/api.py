3a87cbaf49b743b54acc70a69a6705914d25d4af
edna <edna@rr.com>
2018-10-19 07:04:02 +0300

    modify api.py

--- app1/api.py
+++ app1/api.py
@@ -75,8 +75,6 @@ def get_post_sales_record():
 @app.route('/api/v1/sales/<int:record_id>', methods=['GET'])
 def get_single_entry(record_id):
     for record in sales_records:
-        try:
-            record['record_id'] == record_id
+        if record['record_id'] == record_id:
             return jsonify(record), 200
-        except:
-            return jsonify({'message': "no record with such an id"}), 400
+        return jsonify({'message': "no record with such an id"}), 400
