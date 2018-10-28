dcca6327f37e4c7b75fb1d5f0ab2d0fbbb880236
edna <edna@rr.com>
2018-10-28 12:36:22 +0300

    change to OOP

--- project/models.py
+++ project/models.py
@@ -1,20 +1,34 @@
+from flask import jsonify
 products = []
 
 
-class Products:
+class Products():
 
-    def __init__(self, product_id, name, quantity, price):
-        self.product_id = product_id
+    def __init__(self, name, quantity, price):
+        self.product_id = len(products)+1
         self.name = name
         self.quantity = quantity
         self.price = price
 
     def add_product(self):
+
         new_product = {
-            "product_id": len(products)+1,
+            "product_id": self.product_id,
             "name": self.name,
             "quantity": self.quantity,
             "price": self.price
         }
-        return new_product
-        
+
+        products.append(new_product)
+        return jsonify(products)
+
+    @staticmethod
+    def get_product_list():
+        if len(products) > 0:
+            return jsonify(products)
+
+    @staticmethod
+    def get_single_product(product_id):
+        for product in products:
+            if product["product_id"] == product_id:
+                return jsonify(product)
