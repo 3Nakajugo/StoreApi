7187e737efcaa5070af3e413375ee63fb77bd4a0
edna <edna@rr.com>
2018-10-29 08:02:06 +0300

    add validation

--- project/views.py
+++ project/views.py
@@ -21,21 +21,24 @@ def get_products():
 
 @app.route('/api/v1/products', methods=['POST'])
 def post_products():
+    try:
 
-    request_data = request.get_json(force=True)
-    name = request_data["name"]
-    if name == "":
-        return jsonify({"message": "please input product name"}), 400
-    quantity = request_data["quantity"]
-    if quantity == "":
-        return jsonify({"message": "please input product quantity "}), 400
-    price = request_data["price"]
-    if price == "":
-        return jsonify({"message": "please input product price"}), 400
-    new_product = Products(name, quantity, price)
-    n_product = new_product.add_product()
-    if n_product:
-        return n_product, 201
+        request_data = request.get_json(force=True)
+        name = request_data["name"]
+        if name == "":
+            return jsonify({"message": "please input product name"}), 400
+        quantity = request_data["quantity"]
+        if quantity == "":
+            return jsonify({"message": "please input product quantity "}), 400
+        price = request_data["price"]
+        if price == "":
+            return jsonify({"message": "please input product price"}), 400
+        new_product = Products(name, quantity, price)
+        n_product = new_product.add_product()
+        if n_product:
+            return n_product, 201
+    except Exception:
+        return jsonify({"message": "internal server error"}), 500
 
 
 @app.route('/api/v1/products/<int:product_id>', methods=['GET'])
@@ -49,20 +52,23 @@ def get_single_product(product_id):
 @app.route('/api/v1/sales', methods=['POST'])
 def post_sales_record():
     request_data = request.get_json(force=True)
-    date = request_data["date"]
-    if date == "":
-        return jsonify({"message": "please input date"}), 400
-    items = request_data["items"]
-    if items == "":
-        return jsonify({"message": "please input item(s) "}), 400
-    sale_quantity = request_data["sale_quantity"]
-    prices = request_data["prices"]
-    if sale_quantity == "" or prices == "":
-        return jsonify({"message": "please input quantity or price"}), 400
-    new_sales = SaleRecord(date, items, sale_quantity, prices)
-    n_sales = new_sales.add_sale_record()
-    if n_sales:
-        return n_sales, 201
+    try:
+        date = request_data["date"]
+        if date == "":
+            return jsonify({"message": "please input date"}), 400
+        items = request_data["items"]
+        if items == "":
+            return jsonify({"message": "please input item(s) "}), 400
+        sale_quantity = request_data["sale_quantity"]
+        prices = request_data["prices"]
+        if sale_quantity == "" or prices == "":
+            return jsonify({"message": "please input quantity or price"}), 400
+        new_sales = SaleRecord(date, items, sale_quantity, prices)
+        n_sales = new_sales.add_sale_record()
+        if n_sales:
+            return n_sales, 201
+    except Exception:
+        return jsonify({"message": "internal server error"}), 500
 
 
 @app.route('/api/v1/sales', methods=['GET'])
