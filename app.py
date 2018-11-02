de0bb2b7c2b5ad8e909dfe469766ae7f6df4389d
edna <edna@rr.com>
2018-11-02 08:27:50 +0300

    update requirements

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from project.db.datab import Database
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, get_jwt_identity)
+
 
 app = Flask(__name__)
 
